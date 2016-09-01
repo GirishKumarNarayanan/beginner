c770c5e79da586bb82d014cf58c78e8f9996b74f
GirishKumarNarayanan <girishkumar1136@gmail.com>
2016-09-01 22:40:38 +0530

    Add files via upload

---
+++ Element.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Element {
+public static void main(String[] args) {
+Scanner x=new Scanner(System.in);
+String q=x.nextLine();
+String arr[]=q.split(",");
+int i=0;
+String output="";
+int count=0;
+for(i=0;i<arr.length;i++){
+for(int j=0;j<arr.length;j++)
+if(Integer.parseInt(arr[i])==Integer.parseInt(arr[j]))
+count=count+1;	
+if(count==1)
+output=output+arr[i]+" ";
+count=0;
+}
+System.out.println(output);
+}
+}
