e09a7948b00ddc0c6c460d89cade3c55924b374a
GirishKumarNarayanan <girishkumar1136@gmail.com>
2016-09-01 22:54:23 +0530

    Add files via upload

---
+++ Permut.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Permut {
+public static Set<String> repeat(String str)	{
+Set<String> perm=new TreeSet<String>();
+if(str==null){
+return null;
+}
+else if(str.length()==0){
+perm.add("");
+return perm;
+}
+char initial=str.charAt(0);
+String rem=str.substring(1);
+Set<String> words=repeat(rem);
+for(String strNew:words){
+for(int i=0;i<=strNew.length();i++){
+perm.add(charinsert(strNew,initial,i));
+}
+}    
+perm.toString();
+return perm;
+}
+public static String charinsert(String str,char c,int j){
+String begin=str.substring(0,j);
+String end=str.substring(j);
+return begin+c+end;
+}
+public static void main(String args[]){
+Scanner xs=new Scanner(System.in);
+System.out.println("Enter the input: ");
+String input=xs.nextLine();
+Set<String> rep=repeat(input);
+String out="";
+for (Iterator<String> it = rep.iterator(); it.hasNext(); ) {
+String f = it.next();
+out=out+f+" ";
+}
+String str[]=out.split(" ");
+System.out.println(str[0].substring(0,2));
+}	
+}
